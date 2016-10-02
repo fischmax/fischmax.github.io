31c88bac59823281273521ab2557fb245bb2bce9
Maxi <maxi@gfc.strangled.net>
2016-10-02 23:43:34 +0200

    push documentation to winservice-0.1.1

--- doc/search-index.js
+++ doc/search-index.js
@@ -1,3 +1,3 @@
 var searchIndex = {};
-searchIndex['winservice'] = {"items":[[5,"dispatch","winservice","This should never be directly called from the user.",null,null],[5,"serve","","This should never be directly called from the user.",null,null],[14,"Service!","","Create and run the provided function as Windows system service.",null,null]],"paths":[]};
+searchIndex["winservice"] = {"doc":"Run a Windows system service without hassle.","items":[[5,"dispatch","winservice","This should never be directly called from the user.",null,null],[5,"serve","","This should never be directly called from the user.",null,null],[14,"Service","","Create and run the provided function as Windows system service.",null,null]],"paths":[]};
 initSearch(searchIndex);
